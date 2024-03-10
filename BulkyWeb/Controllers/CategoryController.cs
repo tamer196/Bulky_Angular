3d0a629c4262a4dae707d5eb7e59403d94f6c4b7
tamer shamma <tamer.shma19@gmail.com>
2024-03-11 00:04:31 +0300

    Creating the category Controller

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet(Name = "GetCategories")]
+        public IEnumerable<Category> Get()
+        {
+            return _db.Category.ToList();
+        }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+                          builder.WithOrigins("*")
+                                 .AllowAnyMethod()
+                                 .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
